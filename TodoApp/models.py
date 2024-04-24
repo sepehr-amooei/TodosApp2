e331e3fd900db2a24875f320c1e73d5657ecfa2d
sepehr-amooei <sepehramooei@gmail.com>
2024-04-24 11:52:04 +0330

    TodosApp database

---
+++ TodoApp/models.py
@@ -0,0 +1,14 @@
+from dataBase import Base
+from sqlalchemy import Integer, String, Boolean, Column
+
+
+# this will be our todo table
+
+class Todo(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    completed = Column(Boolean, default=False)
